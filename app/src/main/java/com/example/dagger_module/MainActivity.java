3684f4f9e0f4d05613143a27f7190d002eee9c3a
armanziaei <armanziaei83@gmaio.com>
2021-03-29 18:25:22 +0430

    Feat Syncing Dagger With Activity : dagger component created in mainActivity

--- app/src/main/java/com/example/dagger_module/MainActivity.java
+++ app/src/main/java/com/example/dagger_module/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.dagger_module.Dagger.BusinessComponent;
+import com.example.dagger_module.Dagger.DaggerBusinessComponent;
+
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    BusinessComponent component;
+    @Inject
+    Business business;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        component = DaggerBusinessComponent.create();
+        component.inject(this);
+
+        business.StartBusiness();
     }
 }
